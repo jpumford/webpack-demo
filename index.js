46f1a13604e57e2cb3f54f83668168d5be367653
Justin Pumford <jpumford@gmail.com>
2017-11-10 22:56:26 -0700

    step 4: module loading

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('hello world!');
+import { increment, getCounter } from './services/counter';
+
+increment();
+console.log(getCounter());
+increment();
+console.log(getCounter());
---
+++ services/counter.js
@@ -0,0 +1,9 @@
+let i = 0;
+
+export function increment() {
+  i++;
+}
+
+export function getCounter() {
+  return i;
+}
